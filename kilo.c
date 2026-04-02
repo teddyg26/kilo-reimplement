5aaaee9b4adc76898471776b4cc3cbf4644944a6
teddyg26 <teddygarneau@gmail.com>
2026-04-02 16:53:47 -0400

    Added delete key w/o function

--- kilo.c
+++ kilo.c
@@ -18,6 +18,9 @@ enum editorKey {
 	ARROW_RIGHT,
 	ARROW_UP,
 	ARROW_DOWN,
+	DEL_KEY,
+	HOME_KEY,
+	END_KEY,
 	PAGE_UP,
 	PAGE_DOWN
 };
@@ -78,8 +81,13 @@ int editorReadKey(void) {
 				if(read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
 				if(seq[2] == '~') {
 					switch(seq[1]) {
+						case '1': return HOME_KEY;
+						case '3': return DEL_KEY;
+						case '4': return END_KEY;
 						case '5': return PAGE_UP;
 						case '6': return PAGE_DOWN;
+						case '7': return HOME_KEY;
+						case '8': return END_KEY;
 					}
 				}
 			} else {
@@ -88,7 +96,14 @@ int editorReadKey(void) {
 					case 'B': return ARROW_DOWN;
 					case 'C': return ARROW_RIGHT;
 					case 'D': return ARROW_LEFT;
+					case 'H': return HOME_KEY;
+					case 'F': return END_KEY;
 				}
+			}
+		} else if(seq[0] == 'O') {
+			switch(seq[1]) {
+				case 'H': return HOME_KEY;
+				case 'F': return END_KEY;
 			}
 		}
 
@@ -230,6 +245,14 @@ void editorProcessKeypress(void) {
 			write(STDOUT_FILENO, "\x1b[2J", 4); // Clear the screen
 			write(STDOUT_FILENO, "\x1b[H", 3); // Reset the cursor
 			exit(0);
+			break;
+		
+		case HOME_KEY:
+			E.cx = 0;
+			break;
+
+		case END_KEY:
+			E.cx = E.screencols - 1;
 			break;
 		
 		case ARROW_UP:
